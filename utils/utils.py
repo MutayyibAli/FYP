4b1d4b3428c32c4f043a3e6975e57651e1f3136a
Mutayyib Ali <mutayyibali@gmail.com>
2026-04-28 21:15:50 +0500

    .

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def load_config(type):
@@ -10,3 +11,15 @@ def load_config(type):
 def save_config(config, type):
     with open(f"config/{type}.json", "w") as f:
         json.dump(config, f)
+
+
+def get_patient_ids(data_path):
+    # List all patient directories in the data path.
+    patients = [
+        d.split("_")[-1]  # Extract the patient ID from the directory name
+        for d in os.listdir(data_path)  # List all entries in the data path
+        if os.path.isdir(
+            os.path.join(data_path, d)
+        )  # Check if the entry is a directory
+    ]
+    return sorted(patients)
